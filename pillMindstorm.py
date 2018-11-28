a770ffdbab7e6658d612b3ecf1ef6396cea600d8
mmamel <43307993+mmamel@users.noreply.github.com>
2018-11-27 22:17:01 -0800

    New method
    Uses ultrasonic sensor reading to find two containers

--- pillMindstorm.py
+++ pillMindstorm.py
@@ -28,47 +28,50 @@ us = UltrasonicSensor()
 gs = GyroSensor()
 tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
 
-#def move(distance):
-    #some sort of converion of distance with time
-def move_until():
+def Rotate_until_clock(distance):
     us.mode ='US-DIST-CM'
-    tank_drive.on(left_speed=-90,right_speed=90)
+    bool =True;
+    while bool == True:
+        time.sleep(.115)
+        tank_drive.on(left_speed=-8,right_speed=8)
+        measurement= us.distance_centimeters_continuous;
+        print("this is rotate until measurement", measurement)
+        if measurement > distance -1 and measurement < distance +1:
+            tank_drive.off(brake=True)
+            bool = False;
+def Rotate_until_counter(distance):
+    us.mode ='US-DIST-CM'
+    bool =True;
+    while bool == True:
+        time.sleep(.115)
+        tank_drive.on(left_speed=8,right_speed=-8)
+        measurement= us.distance_centimeters_continuous;
+        print("this is rotate until measurement", measurement)
+        if measurement > distance -1 and measurement < distance +1:
+            tank_drive.off(brake=True)
+            bool = False;
+def move_until():
+    bool = True
+    us.mode ='US-DIST-CM'
+    tank_drive.on(left_speed=10,right_speed=10)
     #m1.run_forever(speed_sp=900)
     #m2.run_forever(speed_sp=-900)
 
-    while us.distance_centimeters_continous > 4:
+    while bool == True:
         time.sleep(.1)
+        distance = us.distance_centimeters_continuous
+        print("This is move_until distance: ", distance)
+        if distance < 5:
+            tank_drive.off()
+            bool = False;
     #set up a sleep interval
-    tank_drive.off()
+
         #m1.stop()
         #m2.stop()
         #takes in an angle in radians and converts into degrees
         #spins in a circle forever with intervals of 0.1 sec (time.sleep)
         #and checks when it turned enough using gyro
-def Rotate(angle):
-    #reset gyro
-    angle_degrees = math.degrees(angle)
-    gs.mode = 'GYRO-RATE';
-    gs.mode = 'GYRO-ANG';
-    bool = True;
-    tank_drive.on(left_speed=-90,right_speed=90)
-    #m1.run_forever(speed_sp=900)
-    #m2.run_forever(speed_sp=-900)
-    while bool == True:
-        time.sleep(.1)
-        #tank_pair.on(30,-30)
-        #get angle
-        current_angle=gs.angle;
-        if current_angle == angle_degrees:
-            bool = False;
-            #turn in place
-    tank_drive.off()
-    #m1.stop()
-    #m2.stop()
 
-#creates two arrays and spins indefintely taking measurements at .1 sec increments
-#takes angle and distance which will be used to determine the location of containers
-#by finding the 2 smallest values in the distance array
 def Distance_Array():
     #reset gyro
     gs.mode = 'GYRO-RATE';
@@ -79,9 +82,9 @@ def Distance_Array():
 
     current_angle=0;
     tank_drive.on(left_speed=-2,right_speed=2)
-    while current_angle < 360:
+    while current_angle < 180:
 
-        time.sleep(.12)
+        time.sleep(.115)
         current_angle=gs.angle
         array_angle.append(current_angle)
         #print(current_angle)
@@ -91,15 +94,23 @@ def Distance_Array():
         #tank_pair.on(30,-30)
         #get angle
         distance= us.distance_centimeters_continuous;
-        print(current_angle)
+        print(distance)
         array_distance.append(distance);
     tank_drive.off(brake=True)
 
     return array_distance, array_angle;
+def Reset(distance):
+    bool =True;
 
-#takes in distance array and finds the smallest values
-#returns distance from mindstorm to first container
+    while bool == True:
+        time.sleep(.1)
+        measurement= us.distance_centimeters_continuous;
+        tank_drive.on(left_speed=-5,right_speed=-5)
+        print("this is measurement:", measurement)
 
+        if measurement > distance -1 and measurement < distance +1:
+            tank_drive.off(brake=True)
+            bool = False;
 def Container(array_distance):
 
     length = len(array_distance)
@@ -113,14 +124,9 @@ def Container(array_distance):
             small=array_distance[x]
             #smallest=distance_array[x];
     return small
-    #alternative way? using mod to return back to beggining of array???
-#takes in the distance from first container and initial array
-#and removes the values surronding value of smallest to ensure
-#it can find the second smallest value. It is given that the containers
-#will be seperated by an angle so we can take off multiple values
 def Distance_array_2(smallest, distance_array):
-    index = distance_array.index(smallest)
-    for x in range(1,10):
+    for x in range(1,5):
+        index = distance_array.index(smallest)
         length=len(distance_array);
         if index==length:
             distance_array.pop(index-x)
@@ -134,73 +140,59 @@ def Distance_array_2(smallest, distance_array):
     distance_array.pop(index)
     return distance_array
 
-def Len_Law_Cosine(a,b,angle_C):
-    c = math.sqrt(a ** 2 + b ** 2 - 2 * a * b * math.cos(angle_C));
-    return c
-def Angle_Law_Cosine(z,b,angle_A):
-    init_move = math.sqrt(z**2+b**2-z*b*math.cos(angle_A))
-    return init_move
-def Ambiguous(a,b,c,angle_C):
-    half_pi = (math.pi)/2
-    angle_A = math.asin(a*math.sin(angle_C)/(c))
-    ambiguousangle_A = math.pi - angle_A
-    angle_B = half_pi - angle_A - angle_C
-    ambiguousangle_B = half_pi - math.degrees(ambiguousangle_A) - math.degrees(angle_C)
-    ambiguouslen_b = a*math.sin(angle_B)/math.sin(angle_A)
-    len_b = a*math.sin(ambiguousangle_B)/math.sin(ambiguousangle_A)
-    #find difference then find smallest difference. Float arthmetic is
-    #inaccurate so comaprison will find the smallest absolute difference
-    test_angle_A = math.fabs(b-len_b)
-    test_ambiguousangle_B = math.fabs(b-ambiguouslen_b)
-    if test_angle_A<test_ambiguousangle_B:
-        true_angle=angle_A
-    elif test_angle_A>test_ambiguousangle_B:
-        true_angle = ambiguousangle_B
-    return true_angle
-def main():
-
-    half_pi = math.pi/2
-    array_distance, angle_array=Distance_Array();
-    b=Container(array_distance)
-    #b=Container(array_distance)
-    index_b=array_distance.index(b)
-    array_distance_2=Distance_array_2(b, array_distance)
-    a=Container(array_distance_2)
-    index_a = array_distance.index(a)
-    #line up with first Container
-    turn_angle = math.pi/360*index_b
-    difference_angle = math.fabs(index_b - index_a)
-    print("working1")
-    if difference_angle > half_pi:
-        angle_C = (math.pi)/360 * (360 - difference_angle)
+def colorChecker():
+    bool = True
+    if c1.color == c2.color:
+        print("this is pill color", c1.color)
+        print("this is container collor", c2.color)
+        return bool
     else:
-        angle_C = difference_angle * (math.pi/360)
-    c = Len_Law_Cosine(a,b,angle_C)
-    true_angle = Ambiguous(a,b,c,angle_C)
-    print("working2")
-    z = 0.5*c
-    init_move = Angle_Law_Cosine(z,b,true_angle)
-    angle_G = math.asin(b*math.sin(true_angle)/init_move)
-    angle_M = half_pi - anlge_G - true_angle
-    Rotate(turn_angle)
-    Rotate(angle_M)
-    move(init_move)
-    Rotate(angle_G)
-    move_until()
-    angle_pill_hopper = 11/360
-    #for x in range(10):
-        #pill color
-        #if c1.color== c2.color:
-            #m3.run_to_rel_pos(position_sp=(angle_pill_hopper))
-    #    else:
+        bool = False;
+        print("this is pill color", c1.color)
+        print("this is container collor", c2.color)
+        return bool
+def pillDropper():
+    turn = 360/11
+    m3.on_for_degrees(10, turn, brake=True, block=True)
+def main():
+    bool = True;
+    array_distance, angle_array=Distance_Array();
+    len_First_Container = Container(array_distance)
+    index_first_distance = array_distance.index(len_First_Container)
+    distance_array_2 = Distance_array_2(len_First_Container, array_distance)
+    len_Sec_container = Container(distance_array_2)
+    index_second_distance = array_distance.index(len_Sec_container)
+    print("Computation complete")
+    if index_first_distance<index_second_distance:
+        Rotate_until_counter(len_First_Container)
+    if index_first_distance>index_second_distance:
+        Rotate_until_counter(len_Sec_container)
 
-        #    m1.run_timed(time_sp=1000, speed_sp=-750)
-        #    m2.run_timed(time_sp=1000, speed_sp=-750)
-        #    Rotate(half_pi)
-            #move_until()
-        #    m3.run_to_rel_pos(position_sp=(angle_pill_hopper))
-
-
+    count = 0
+    while bool == True:
+        us.mode ='US-DIST-CM'
+        reset_distance = us.distance_centimeters
+        move_until()
+        drop = colorChecker()
+        if drop == True or c2.color == 1:
+            tank_drive.off(brake=True)
+            count = count +1
+            pillDropper()
+            Reset(reset_distance)
+        if drop == False:
+            Reset(reset_distance)
+            if count%2 != 0:
+                Rotate_until_clock(len_Sec_container)
+                move_until()
+                count = count +1
+                pillDropper()
+            if count%2 == 0:
+                Rotate_until_counter(len_First_Container)
+                move_until()
+                count = count +1
+                pillDropper()
+        if count == 10:
+            bool = False
 
 
 main();
